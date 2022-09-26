9ea6891d3fecb30f826f5a4808c995550ef451f0
Italo Fernandes <italojafernandes@hotmail.com>
2022-09-26 17:59:59 -0300

    :sparkles: Criando a primeira pagina para testar funcionamento do NextJS

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function pages() {
+  return (
+    <div>
+        <h1>Alura Cases - Home</h1>
+    </div>
+  );
+}
+
+export default pages;
