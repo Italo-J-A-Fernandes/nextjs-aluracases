8595c3fdf7f721187f0938da0ffac58ca9e043ae
Italo Fernandes <italojafernandes@hotmail.com>
2022-09-28 11:42:31 -0300

    :lipstick: Trabalhando com estilos em componentes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,26 @@
 import Link from "../components/Link";
 
+const Title = ({children, type}) => { 
+  const Tag = type
+  return (
+    <>
+      <Tag>
+        {children}
+      </Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: green;
+          font-family: sans-serif;
+        }
+      `}</style>
+    </>
+  )
+}
+
 const HomePage = () => {
   return (
     <div>
-        <h1>Alura Cases - Home Page</h1>
+        <Title type="h1">Alura Cases - Home Page</Title>
         <Link href="/faq">
           Ir para FAQ
         </Link>
