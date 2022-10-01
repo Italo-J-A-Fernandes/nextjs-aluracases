9655d24a16bb5ff1087dc39e0f4796f2581ef0b4
Italo Fernandes <italojafernandes@hotmail.com>
2022-09-30 23:40:03 -0300

    :sparkles: Separando stylled global

---
+++ src/theme/GlobalStyled.js
@@ -0,0 +1,11 @@
+const GlobalStyled = () => {
+    return (
+      <style global jsx>{`
+        body {
+          font-family: sans-serif;
+        }
+      `}</style>
+    )
+}
+
+export default GlobalStyled;
