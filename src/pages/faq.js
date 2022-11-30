267eda6818d316ffe62ee1ad94a2f08d2e33ed66
Italo Fernandes <italojafernandes@hotmail.com>
2022-11-30 18:08:23 -0300

    :hammer: Diferença entre getStaticProps e getServerSideProps

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,6 +1,10 @@
 import Link from "../components/Link";
 
-export async function getStaticProps() {
+// getStaticProps para conteúdos que nunca vai mudar de fato estaticos que serão gerados somente no build do projeto
+// getServerSideProps pegar alterações, roda a cada novo acesso
+// em modo dev sempre roda a cada acesso
+
+export async function getServerSideProps() {
   const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
   const faq = await fetch(FAQ_API_URL).then((response) => {
     return response.json()
