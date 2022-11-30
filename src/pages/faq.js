c3175def257370031d9974ba97b22df4b4b81715
Italo Fernandes <italojafernandes@hotmail.com>
2022-11-30 01:09:34 -0300

    :sparkles: Utilizando getStaticProps do NextJS

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,17 +1,20 @@
-import { useEffect, useState } from "react";
 import Link from "../components/Link";
 
-const FAQPage = () => {
-  const [faq, setFaq] = useState([]);
-  useEffect(() => {
-    const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
-    fetch(FAQ_API_URL).then((response) => {
-      return response.json()
-    }).then((response) => {
-      console.log(response);
-      setFaq(response)
-    })
-  }, [])
+export async function getStaticProps() {
+  const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+  const faq = await fetch(FAQ_API_URL).then((response) => {
+    return response.json()
+  }).then((response) => {
+    return response;
+  })
+  return {
+    props: {
+      faq
+    },
+  }
+}
+
+const FAQPage = ({ faq }) => {
   return (
     <div>
         <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
