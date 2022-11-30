7d3197a3ec2e5798f084e72ff3470e82548248d3
Italo Fernandes <italojafernandes@hotmail.com>
2022-11-30 09:56:14 -0300

    :hammer: Pegando dados da api fake e exibindo na tela, de forma tradicional.

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import Link from "../components/Link";
 
 const FAQPage = () => {
+  const [faq, setFaq] = useState([]);
+  useEffect(() => {
+    const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+    fetch(FAQ_API_URL).then((response) => {
+      return response.json()
+    }).then((response) => {
+      console.log(response);
+      setFaq(response)
+    })
+  }, [])
   return (
     <div>
         <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
         <Link href="/">
             Ir para Home
         </Link>
+        <ul>
+          {faq.map(({question, answer}) => (
+            <li key={question}>
+              <article>
+                <h2>{question}</h2>
+                <p>{answer}</p>
+              </article>
+            </li>
+          ))}
+        </ul>
     </div>
   );
 }
