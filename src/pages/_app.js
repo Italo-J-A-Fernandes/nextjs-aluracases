bc336d25d0c734f5fdc8c477331d2b7bfbc468ed
Italo Fernandes <italojafernandes@hotmail.com>
2022-11-30 22:37:17 -0300

    :sparkles: Adicionando fonts externas no projeto

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
+import Head from 'next/head';
 import GlobalStyled from '../theme/GlobalStyled'
 
 function MyApp({Component, pageProps}) {
   return (
     <>
-        <GlobalStyled />
-        <Component {...pageProps} />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+      </Head>
+      <GlobalStyled />
+      <Component {...pageProps} />
     </>
   )
 }
